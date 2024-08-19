ce7aa52da1bcd14240622531fef48e63a8d0075e
MeloknightIX <meloknightix@gmail.com>
2024-08-19 22:39:20 +0200

    updated project structure -> moved styles into src/views/styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM, { Container } from "react-dom/client";
-import "./index.css";
+import "./views/styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Container);
