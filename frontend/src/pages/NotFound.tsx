0ba7c474970c18b2dec830f6ded61e8b9f0e2581
MeloknightIX <meloknightix@gmail.com>
2024-11-20 22:43:45 +0100

    add isOffline to not-found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import FlexContainer from "../components/FlexContainer";
+import useIsOffline from "../utils/useIsOffline";
 
 const NotFound = () => {
+  const { isOffline } = useIsOffline();
   return (
-    <div>
+    <FlexContainer>
       <h1>Not Found</h1>
       <p>It appears the page you are looking for could not be found. </p>
-      <p>This error may occur if you are offline. </p>
+      {isOffline && <p>This error may be occuring because you are offline. </p>}
       <Link to="/">Navigate back to the homepage</Link>
-    </div>
+    </FlexContainer>
   );
 };
 
