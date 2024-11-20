bb59b9b9a16459f89074ce79ea7bdab83ec83f7d
MeloknightIX <meloknightix@gmail.com>
2024-11-20 17:26:44 +0100

    add getMaxId util

--- frontend/src/utils/getMaxId.ts
+++ frontend/src/utils/getMaxId.ts
@@ -1,12 +1,12 @@
 type WithId = { id: number };
 
-const getNewId = <T extends WithId>(data: T[]): number => {
+const getMaxId = <T extends WithId>(data: T[]): number => {
   var maxId = -1;
   data.forEach((d) => {
     if (d.id > maxId) {
       maxId = d.id;
     }
   });
-  return maxId + 1;
+  return maxId;
 };
-export default getNewId;
+export default getMaxId;
